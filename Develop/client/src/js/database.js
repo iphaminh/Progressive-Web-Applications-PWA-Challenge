3f8f3ab5642bd619220c1678fa58bf69884f56c2
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-02 15:46:18 -0500

    setup database.js for get and put transaction

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,41 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { console.error('putDb not implemented');
+// Open (or create) the 'jate' database with version 1.
+const db = await openDB('jate', 1);
+
+// Start a transaction on the 'jate' object store with readwrite permissions.
+// This allows us to modify the data.
+const tx = db.transaction('jate', 'readwrite');
+
+// Access the 'jate' object store within the transaction.
+const store = tx.objectStore('jate');
+
+// Add or update the content in the object store.
+// If a record with the same key exists, it will be updated. Otherwise, a new record will be added.
+await store.put({ content: content });
+
+// Wait for the transaction to complete and then return its status.
+return tx.done;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {console.error('getDb not implemented');
+// Open (or create) the 'jate' database with version 1.
+const db = await openDB('jate', 1);
 
+// Start a transaction on the 'jate' object store with readonly permissions.
+// This allows us to fetch the data without modifying it.
+const tx = db.transaction('jate', 'readonly');
+
+// Access the 'jate' object store within the transaction.
+const store = tx.objectStore('jate');
+
+// Retrieve all records from the object store.
+const allContent = await store.getAll();
+
+// Return all the fetched content.
+return allContent;
+};
 initdb();

33dd8e683a1ee995c4fe37cf1ccf744bc8d45794
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-03 21:24:45 -0500

    undo all

--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -25,35 +25,17 @@ export default class {
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      console.log('Data retrieved from DB:', data);
-    
-      // Ensure that data.value is a string before setting it as the editor value
-      if (data && typeof data.value !== 'string') {
-        console.error('Data.value from IndexedDB is not a string:', data.value);
-        data.value = '';
-      }
-    
-      // Ensure that localData is a string
-      const localDataStr = typeof localData === 'string' ? localData : '';
-    
-      this.editor.setValue(data?.value || localDataStr || header);
-}).catch(error => {
-  // Log any errors from getDb
-  console.error('Error getting data from IndexedDB:', error);
-});
-    
+      this.editor.setValue(data || localData || header);
+    });
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself loses focus
+    // Save the content of the editor when the editor itself is loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content')).catch(error => {
-        // Log any errors from putDb
-        console.error('Error saving data to IndexedDB:', error);
-      });
+      putDb(localStorage.getItem('content'));
     });
   }
 }

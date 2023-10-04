3deafb13a549c2d49639c37d55970b94c43f105b
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-03 20:16:40 -0500

    remoce catch in editor

--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -24,34 +24,18 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-
-      // Ensure that data is a string before setting it as the editor value
-      if (typeof data !== 'string') {
-        console.error('Data from IndexedDB is not a string:', data);
-        data = '';
-      }
-
-      // Ensure that localData is a string
-      const localDataStr = typeof localData === 'string' ? localData : '';
-
-      this.editor.setValue(data || localDataStr || header);
-    }).catch(error => {
-      // Log any errors from getDb
-      console.error('Error getting data from IndexedDB:', error);
+       console.info('Loaded data from IndexedDB, injecting into editor');
+      this.editor.setValue(data || localData || header);
     });
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself loses focus
+    // Save the content of the editor when the editor itself is loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content')).catch(error => {
-        // Log any errors from putDb
-        console.error('Error saving data to IndexedDB:', error);
-      });
+      putDb(localStorage.getItem('content'));
     });
   }
 }
